20dadcf147a0d5999136816a3b5cb15eb6a9e0ea
Christian List <dragondriver8@outlook.com>
2026-02-01 21:51:44 -0500

    In NanoVGUI.java add more function for easy shape, text, and icon making.

--- src/main/java/org/bigacl/renderEngine/gui/font/NanoVGUI.java
+++ src/main/java/org/bigacl/renderEngine/gui/font/NanoVGUI.java
@@ -1,12 +1,14 @@
 package org.bigacl.renderEngine.gui.font;
 
+import org.bigacl.renderEngine.utils.consts.Const;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
 import org.lwjgl.nanovg.NVGColor;
 import org.lwjgl.system.MemoryStack;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.nio.file.Files;
 
 import static org.lwjgl.nanovg.NanoVG.*;
 import static org.lwjgl.nanovg.NanoVGGL3.*;
@@ -79,16 +81,66 @@ public class NanoVGUI {
     nvgEndFrame(vg);
   }
 
-  public void drawText(String text, float x, float y, float size, float r, float g, float b) {
+  /**
+   * Draw Text Function spreadOut
+   * @param text = Text show on screen.
+   * @param x = X position of text.
+   * @param y = Y position of text.
+   * @param size = Size of text.
+   * @param redColor = Red color of text.
+   * @param greenColor = Green colro of text,
+   * @param blueColor = Blue color of text.
+   */
+  public void drawText(String text, float x, float y, float size, float redColor, float greenColor, float blueColor) {
     try (MemoryStack stack = MemoryStack.stackPush()) {
       NVGColor color = NVGColor.malloc(stack);
-      nvgRGBAf(r, g, b, 1.0f, color);
+      nvgRGBAf(redColor, greenColor, blueColor, 1.0f, color);
 
       nvgFontFace(vg, "sans");
       nvgFontSize(vg, size);
       nvgFillColor(vg, color);
       nvgText(vg, x, y, text);
     }
+  }
+
+  /**
+   * Draw Function short
+   * @param text = Text show on screen.
+   * @param position = Position of text.
+   * @param size = Size of text.
+   * @param color = Color of text.
+   */
+  public void drawText(String text, Vector2f position, float size, Vector3f color){
+    drawText(text, position.x, position.y, size, color.x, color.y, color.z);
+  }
+
+  /**
+   * Draw Text function with position short.
+   * @param text = Text show on screen.
+   * @param startPos = Start position on screen.
+   * @param position = Item position in screen.
+   * @param size = Size of text.
+   * @param color = Color of text.
+   */
+  public void drawText(String text, Vector2f startPos, int position, float size, Vector3f color){
+    drawText(text, startPos.x, (startPos.y + (position * (size + Const.PADDING))), size, color.x, color.y, color.z);
+  }
+
+  /**
+   * Draw Text Function with position long.
+   * @param text = Text show on screen.
+   * @param x = Starting x position.
+   * @param y = Starting y position.
+   * @param position = Item position in screen.
+   * @param size = Size of text.
+   * @param redColor = Red Color of text.
+   * @param greenColor = Green color of text.
+   * @param blueColor = Blue color of Text
+   */
+  public void drawText(String text, float x, float y, int position, float size, float redColor, float greenColor, float blueColor){
+    Vector2f startPos = new Vector2f(x,y);
+    Vector3f color = new Vector3f(redColor, greenColor, blueColor);
+    drawText(text, startPos.x, (startPos.y + (position * (size + Const.PADDING))), size, color.x, color.y, color.z);
   }
 
   public void drawNerdIcon(String icon, float x, float y, float size, float r, float g, float b) {
@@ -115,8 +167,7 @@ public class NanoVGUI {
     }
   }
 
-  public void drawRoundedRect(float x, float y, float w, float h, float radius,
-                              float r, float g, float b, float a) {
+  public void drawRoundedRect(float x, float y, float w, float h, float radius, float r, float g, float b, float a) {
     try (MemoryStack stack = MemoryStack.stackPush()) {
       NVGColor color = NVGColor.malloc(stack);
       nvgRGBAf(r, g, b, a, color);
@@ -128,6 +179,55 @@ public class NanoVGUI {
     }
   }
 
+
+  /**
+   * Draw Nerd icon with text next to it
+   * @param icon = Nerd font icon (e.g., "", "")
+   * @param text = Text to display next to icon
+   * @param x = X position
+   * @param y = Y position
+   * @param iconSize = Size of the icon
+   * @param textSize = Size of the text
+   * @param r = Red color
+   * @param g = Green color
+   * @param b = Blue color
+   */
+  public void drawIconWithText(String icon, String text, float x, float y, float iconSize, float textSize, float r, float g, float b) {
+    // Draw icon
+    drawNerdIcon(icon, x, y, iconSize, r, g, b);
+
+    // Draw text next to icon (offset by icon size + padding)
+    float textX = x + iconSize + Const.PADDING;
+    drawText(text, textX, y, textSize, r, g, b);
+  }
+
+  /**
+   * Draw Nerd icon with text - Vector version
+   * @param icon = Nerd font icon
+   * @param text = Text to display
+   * @param position = Position vector
+   * @param iconSize = Size of icon
+   * @param textSize = Size of text
+   * @param color = RGB color vector
+   */
+  public void drawIconWithText(String icon, String text, Vector2f position, float iconSize, float textSize, Vector3f color) {
+    drawIconWithText(icon, text, position.x, position.y, iconSize, textSize, color.x, color.y, color.z);
+  }
+
+  /**
+   * Draw Nerd icon with text - With position index
+   * @param icon = Nerd font icon
+   * @param text = Text to display
+   * @param startPos = Starting position
+   * @param position = Item position index
+   * @param iconSize = Size of icon
+   * @param textSize = Size of text
+   * @param color = RGB color vector
+   */
+  public void drawIconWithText(String icon, String text, Vector2f startPos, int position, float iconSize, float textSize, Vector3f color) {
+    float y = startPos.y + (position * (Math.max(iconSize, textSize) + Const.PADDING));
+    drawIconWithText(icon, text, startPos.x, y, iconSize, textSize, color.x, color.y, color.z);
+  }
   public void cleanup() {
     nvgDelete(vg);
   }
