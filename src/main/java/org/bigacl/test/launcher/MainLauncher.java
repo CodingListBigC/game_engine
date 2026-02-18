3227869c8b362f697191e5da789e8b41255c7c11
Christian List <dragondriver8@outlook.com>
2026-02-18 12:13:46 -0500

    Update files for better reading with more comments

--- src/main/java/org/bigacl/test/launcher/MainLauncher.java
+++ src/main/java/org/bigacl/test/launcher/MainLauncher.java
@@ -1,13 +1,8 @@
 package org.bigacl.test.launcher;
 
-import org.bigacl.renderEngine.MainGame;
-import org.bigacl.renderEngine.camera.Camera;
-import org.bigacl.renderEngine.gui.font.NanoVGUI;
 import org.bigacl.renderEngine.item.ItemManger;
 import org.bigacl.renderEngine.item.placeable.aparment.Aparment;
 import org.bigacl.renderEngine.item.placeable.house.House;
-import org.bigacl.renderEngine.logic.IGameLogic;
-import org.bigacl.renderEngine.player.level.MainLevel;
 import org.bigacl.renderEngine.shaders.ShaderMaster;
 import org.bigacl.renderEngine.utils.consts.ClassConst;
 import org.bigacl.renderEngine.window.WindowMaster;
@@ -15,20 +10,20 @@ import org.joml.Vector3f;
 
 public class MainLauncher {
   public static void main(String[] args) {
-    // Window Init
+    // Get class from consts
     WindowMaster window = ClassConst.window;
-
     ShaderMaster shaderMaster = ClassConst.shader3d;
-    House house = new House();
-
-    // Item Manger Iit
     ItemManger itemManger = ClassConst.itemManger;
+
+    // Add House item
+    House house = new House();
     itemManger.addItem(house);
+
+    // Add Aparment Item
     Aparment aparment = new Aparment();
     aparment.place(new Vector3f(10,0,10), 0);
     itemManger.addItem(aparment);
-    NanoVGUI nanoVGUI = new NanoVGUI();
-    NanoVGUI NanoVGUI = new NanoVGUI();
+
     // Render loop
     window.loop();
 
