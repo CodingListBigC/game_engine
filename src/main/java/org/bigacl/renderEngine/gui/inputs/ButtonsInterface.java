708637039fd4189f01dd457bf138403b50118dfa
Christian List <dragondriver8@outlook.com>
2026-02-20 13:04:57 -0500

    Remove ButtonInterface

--- src/main/java/org/bigacl/renderEngine/gui/inputs/ButtonsInterface.java
+++
@@ -1,6 +0,0 @@
-package org.bigacl.renderEngine.gui.inputs;
-
-public interface ButtonsInterface {
-  public void checkButtonInput(double mouseX, double mouseY, int action);
-  public void render();
-}
