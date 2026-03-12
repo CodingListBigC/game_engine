474aed1675e5175d404639a8c2b4395e46d32c63
Christian List <dragondriver8@outlook.com>
2026-03-12 12:43:05 -0400

    feat: add min and max numbers.
    - SnapNumbers are not min and max numbers.

--- src/main/java/org/bigacl/renderEngine/utils/number/SnapNumbers.java
+++ src/main/java/org/bigacl/renderEngine/utils/number/SnapNumbers.java
@@ -3,6 +3,8 @@ package org.bigacl.renderEngine.utils.number;
 public class SnapNumbers {
   float changeAmount;
   float currentAmount;
+  float minValue = 1;
+  float maxValue = -1;
 
   public SnapNumbers(float changeAmount, float currentAmount) {
     this.changeAmount = changeAmount;
@@ -11,10 +13,12 @@ public class SnapNumbers {
 
   public void addChangeAmount() {
     this.currentAmount += changeAmount;
+    checkValues();
   }
 
   public void subtractChangeAmount() {
     this.currentAmount -= changeAmount;
+    checkValues();
   }
 
   public float getChangeAmount() {
@@ -23,6 +27,7 @@ public class SnapNumbers {
 
   public void setChangeAmount(float changeAmount) {
     this.changeAmount = changeAmount;
+    checkValues();
   }
 
   public float getCurrentAmount() {
@@ -31,10 +36,20 @@ public class SnapNumbers {
 
   public void setCurrentAmount(float currentAmount) {
     this.currentAmount = currentAmount;
+    checkValues();
   }
 
   public void changeByInt(int changeInt) {
     this.currentAmount += this.changeAmount * changeInt;
-
+    checkValues();
   }
+
+  private void checkValues() {
+    if (this.currentAmount < minValue) this.currentAmount = minValue;
+    if (this.changeAmount < minValue) this.changeAmount = minValue;
+    if (this.maxValue == -1) return;
+    if (this.maxValue <= this.currentAmount) this.currentAmount = this.maxValue;
+    if (this.maxValue <= this.changeAmount) this.changeAmount = this.maxValue;
+  }
+
 }
